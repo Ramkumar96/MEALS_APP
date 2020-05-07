d2afea4b0b4c7d8de6208ad722752752faf4a7ba
Ramkumar96 <sivaram0203@gmail.com>
2020-05-07 23:56:14 +0530

    MeaLs describtion screen

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
         alignItems : 'flex-end'
     },
     title:{
-        fontFamily : 'open-sans-bold',
+        // fontFamily : 'open-sans-bold',
         fontSize : 18,
         textAlign : 'right'
     }
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import {View, Text ,Button,  StyleSheet} from 'react-native';
+import {View, Text ,FlatList,  StyleSheet} from 'react-native';
  
-import {CATEGORIES} from '../data/dummy-data';
+import {CATEGORIES , MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = props => {
 
+    const renderMealItem = itemData => {
+        return <MealItem title={itemData.item.title} 
+                        image={itemData.item.imageUrl}
+                        duration ={itemData.item.duration}
+                        complexity ={itemData.item.complexity}
+                        affordability ={itemData.item.affordability}
+                        onSelectMeal={() => {
+                            props.navigation.navigate({
+                                routeName : 'MealDetail',
+                                params:{
+                                    mealId : itemData.item.id
+                                }
+                                    })
+                        }}/>
+    };
+
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
-    
+    const displayedMeals = MEALS.filter(
+        meal => meal.categoryIds.indexOf(catId) >= 0
+    );
+
+
     return(
         <View style={styles.screen}>
-            <Text>The Category Meal Screen !</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="Go to Meals Details" onPress={() => {
-                props.navigation.navigate({routeName: 'MealDetail'})
-            }}/>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item,index) => item.id}
+                renderItem={renderMealItem}
+                style={{width: '100%'}}/>
         </View>
 
     );
@@ -37,6 +57,7 @@ const styles = StyleSheet.create({
         flex : 1,
         justifyContent : 'center',
         alignItems: 'center',
+        margin: 15
     }
 });
 
